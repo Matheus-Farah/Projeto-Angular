00a04811428c3142ddd5d4db2b1ce6f72b03c7e5
Matheus-Farah <matheusfarah00@gmail.com>
2022-08-03 17:13:08 -0300

    Primeito commit do projeto

---
+++ Primeiro-Projeto/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentsDataComponent } from './components/parents-data/parents-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponentComponent,
+    ParentsDataComponent,
+    DirectivesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
