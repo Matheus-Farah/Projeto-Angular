ca0c04e1d204a5e43d3ae6f353d78bc5d8793604
Matheus-Farah <matheusfarah00@gmail.com>
2022-08-11 17:43:23 -0300

    alteracoes nas paginas, adicao de rotas

--- Primeiro-Projeto/src/app/app-routing.module.ts
+++ Primeiro-Projeto/src/app/app-routing.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentsDataComponent } from './components/parents-data/parents-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBidingComponent } from './components/two-way-biding/two-way-biding.component';
+
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent}, //Criando o primeiro componente
+  {path: 'parent', component: ParentsDataComponent}, //Compartilhamento de dados entre componentes pai e filho @Input
+  {path: 'directive', component: DirectivesComponent}, //Criando e testando diretivas 
+  {path: 'if', component: IfRenderComponent}, //Aprendendo renderização condicional com If e Else
+  {path: 'eventos', component: EventosComponent}, //Criando eventos com o Click
+  {path: 'emitter', component: EmitterComponent}, //Vai realizar uma determinada função após receber uma informação emitida pelo componente filho @Output
+  {path: 'list', component: ListRenderComponent}, //Rendereização de uma lista e utilização de interface
+  {path: 'pipes', component: PipesComponent}, //Utilizando Pipe Operators 
+  {path: 'two', component: TwoWayBidingComponent}, //Utilização de form, importação do module
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- Primeiro-Projeto/src/app/app.module.ts
+++ Primeiro-Projeto/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentsDataComponent } from './components/parents-data/parents-data.component';
@@ -10,6 +11,9 @@ import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBidingComponent } from './components/two-way-biding/two-way-biding.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,15 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     IfRenderComponent,
     EventosComponent,
     EmitterComponent,
-    ChangeNumberComponent
+    ChangeNumberComponent,
+    ListRenderComponent,
+    PipesComponent,
+    TwoWayBidingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
